452f8c08bb31a6ce68617975f189e5be7c26b793
Sean King <kingsc@miamioh.edu>
2017-09-29 15:10:05 -0400

    build(Firebase): Add and setup firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import firebase from 'firebase'
 import Header from './components/common/Header'
+
 
 class App extends Component {
   componentWillMount() {
-
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDDiCpz5R42q8NK2KlM-F-j5c_f5ENtNec',
+      authDomain: 'rn-auth-9329f.firebaseapp.com',
+      databaseURL: 'https://rn-auth-9329f.firebaseio.com',
+      projectId: 'rn-auth-9329f',
+      storageBucket: 'rn-auth-9329f.appspot.com',
+      messagingSenderId: '530622078948'
+    })
   }
 
   render() {
