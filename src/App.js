58b3213a9b94bfaf4a0e82ba37af8d95e1cdbbb5
Sean King <kingsc@miamioh.edu>
2017-10-01 21:29:18 -0400

    feat(Logout): Add logout logic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import firebase from 'firebase'
 import { View } from 'react-native'
 import Header from './components/common/Header'
+import Button from './components/common/Button'
 import LoginForm from './components/LoginForm'
 
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDDiCpz5R42q8NK2KlM-F-j5c_f5ENtNec',
@@ -15,6 +20,14 @@ class App extends Component {
       storageBucket: 'rn-auth-9329f.appspot.com',
       messagingSenderId: '530622078948'
     })
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
   }
 
   render() {
@@ -23,7 +36,15 @@ class App extends Component {
         <Header
           text={'Authentication'}
         />
-        <LoginForm />
+        {!this.state.loggedIn ? (
+          <LoginForm />
+        ) : (
+          <Button
+            onPress={() => firebase.auth().signOut()}
+          >
+            Log Out
+          </Button>
+        )}
       </View>
 
     )
